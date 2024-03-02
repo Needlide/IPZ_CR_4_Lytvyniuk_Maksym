927f117525519ba6f5bc02aef17ffa837cbe18a3
Needlide <max.litvinyuck553@gmail.com>
2024-03-02 13:56:21 +0000

    Added TextDetail function, which will be used on the second screen

--- app/src/main/java/ua/edu/lntu/ipz_cw_4/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/ipz_cw_4/MainActivity.kt
@@ -1,19 +1,34 @@
 package ua.edu.lntu.ipz_cw_4
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import ua.edu.lntu.ipz_cw_4.ui.theme.IPZ_CR_4Theme
 
 class MainActivity : ComponentActivity() {
@@ -35,10 +50,11 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ItemsList(modifier: Modifier = Modifier) {
-    LazyColumn(contentPadding = PaddingValues(8.dp)){
-        items(10) {
-            
-        }
-    }
+fun TextDetail(text: String) {
+    Text(
+        text = text,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    )
 }
