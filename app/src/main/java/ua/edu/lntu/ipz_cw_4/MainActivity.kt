2f8efbdb9c5b0f3eb25a6e1a8f03390dfeb71eda
Needlide <max.litvinyuck553@gmail.com>
2024-03-02 12:49:50 +0000

    Added LazyColumn, configured content padding, added empty 10 items inside

--- app/src/main/java/ua/edu/lntu/ipz_cw_4/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/ipz_cw_4/MainActivity.kt
@@ -3,18 +3,23 @@ package ua.edu.lntu.ipz_cw_4
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ua.edu.lntu.ipz_cw_4.ui.theme.IPZ_CR_4Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             IPZ_CR_4Theme {
                 // A surface container using the 'background' color from the theme
@@ -22,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ItemsList()
                 }
             }
         }
@@ -30,17 +35,10 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    IPZ_CR_4Theme {
-        Greeting("Android")
+fun ItemsList(modifier: Modifier = Modifier) {
+    LazyColumn(contentPadding = PaddingValues(8.dp)){
+        items(10) {
+            
+        }
     }
 }
